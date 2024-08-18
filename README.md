6093c1820ea4c3442c7421f38e398c5357462ce3
sheytoonch <83016208+sheytoonch@users.noreply.github.com>
2024-08-18 11:17:04 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# tic-tac-toe-limitless
+ Play your bet until you reach stroke of 5 in any direction.
---
+++ script.js
@@ -0,0 +1,128 @@
+window.addEventListener('load', () => {
+    const canvas = document.getElementById('canvas1');
+    const ctx = canvas.getContext('2d');
+
+    let CANVAS_WIDTH = canvas.getBoundingClientRect().width;
+    let CANVAS_HEIGHT = canvas.getBoundingClientRect().height;
+
+    const orthogonalNeighborVectors = [[0, -1], [0, 1], [-1, 0], [1, 0]];
+    const diagonalNeighborVectors = [[-1, -1], [1, -1], [-1, 1], [1, 1]];
+    let gameOver = false;
+
+    const screenInfoH2 = document.getElementById('screenInfoH2'); // TODO: delete later
+    let iterationsToRefreshFPS = 50;
+
+
+    class InputHandler {
+        constructor() {
+            this.keys = [];
+            window.addEventListener('keydown', e => { // if key is clicked
+                if ((   e.key === 'w'  ||
+                        e.key === 'a'  ||
+                        e.key === 's'  ||
+                        e.key === 'd') &&
+                        this.keys.indexOf(e.key) === -1) {
+                    this.keys.push(e.key);
+                }
+            });
+            window.addEventListener('keyup', e => { // if key is released
+                if (    e.key === 'w'  ||
+                        e.key === 'a'  ||
+                        e.key === 's'  ||
+                        e.key === 'd') {
+                    this.keys.splice(this.keys.indexOf(e.key), 1);
+                }
+            });
+        }
+    }
+
+
+    class Game {
+        constructor() {
+            this.x = 0;
+            this.y = 0;
+            this.minX = 0;
+            this.maxX = 0;
+            this.minY = 0;
+            this.maxY = 0;
+            this.visualScale = 1;
+            this.players = ['x', 'o'];
+            this.currentPlayer = this.players[ Math.random() > 0.5 ? 0 : 1 ];
+            this.turnFinished = false;
+            this.fields = [new Field(0, 0, this.currentPlayer)];
+        }
+
+        nextTurn() {
+
+        }
+
+        update(input, deltaTime) {
+            //if(input.keys.includes('w')) this.y -= deltaDistance;
+            //if(input.keys.includes('a')) this.x -= deltaDistance;
+            //if(input.keys.includes('s')) this.y += deltaDistance;
+            //if(input.keys.includes('d')) this.x += deltaDistance;
+        }
+
+        draw() {
+
+        }
+
+        addField() {
+            
+        }
+    }
+
+
+    class Player {
+        constructor(i) {
+            this.i = i;
+        }
+    }
+
+
+    class Field {
+
+
+        constructor(x, y) {
+            this.x = x;
+            this.y = y;
+            this.visited = false;
+            this.size = 200;
+            this.image = new Image();
+        }
+    }
+
+
+    const input = new InputHandler();
+    const game = new Game();
+
+    let lastTime = 0;
+
+    function animate(timestamp) {
+        const deltaTime = timestamp - lastTime;
+        lastTime = timestamp;
+
+        CANVAS_WIDTH = canvas.getBoundingClientRect().width;
+        CANVAS_HEIGHT = canvas.getBoundingClientRect().height;
+
+        // wait for mouse click
+        // if mouse click: game...
+
+
+        game.update(input, deltaTime);
+        game.draw();
+
+
+        // TODO: delete if statement below
+        if (iterationsToRefreshFPS > 0) {
+            iterationsToRefreshFPS--;
+        } else {
+            screenInfoH2.innerText = `Width: ${CANVAS_WIDTH} x Height: ${CANVAS_HEIGHT} | FPS: ${Math.floor(1000/deltaTime)}, deltaTime: ${deltaTime}`; // TODO: delete later
+            iterationsToRefreshFPS = 50;
+        }
+        requestAnimationFrame(animate);
+    }
+
+    animate(0);
+
+});
